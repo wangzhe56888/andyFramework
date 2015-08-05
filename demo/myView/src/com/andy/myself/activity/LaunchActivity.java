20d93bed7a3fe3cd294ad8f0f04dbf870ad8b955
wangzhe56888 <win58@qq.com>
2015-08-05 17:35:02 +0800

    modify launch activity

--- demo/myView/src/com/andy/myself/activity/LaunchActivity.java
+++ demo/myView/src/com/andy/myself/activity/LaunchActivity.java
@@ -4,12 +4,15 @@ import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.Animation.AnimationListener;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -27,6 +30,7 @@ public class LaunchActivity extends BaseActivity implements AnimationListener{
 	
 	ArrayList<TextView> textViews = new ArrayList<TextView>(7);
 	private AlphaAnimation textAnimation;
+	private EditText passwordEditText;
 	
 	private int index = 0;
 	private AnimationListener textAnimationListener = new AnimationListener() {
@@ -53,6 +57,8 @@ public class LaunchActivity extends BaseActivity implements AnimationListener{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_launch);
 		
+		passwordEditText = (EditText) findViewById(R.id.launch_password_id);
+		
 		textViews.add((TextView) findViewById(R.id.launch_text_1));
 		textViews.add((TextView) findViewById(R.id.launch_text_2));
 		textViews.add((TextView) findViewById(R.id.launch_text_3));
@@ -69,11 +75,33 @@ public class LaunchActivity extends BaseActivity implements AnimationListener{
 		
 		RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.launch_layout_id);
 		AlphaAnimation alphaAnimation = new AlphaAnimation(0.3f, 1.0f);
-		alphaAnimation.setDuration(1500);
+		alphaAnimation.setDuration(1000);
 		alphaAnimation.setInterpolator(new AccelerateDecelerateInterpolator());
 		relativeLayout.setAnimation(alphaAnimation);
 		alphaAnimation.setAnimationListener(this);
 		alphaAnimation.start();
+		
+		passwordEditText.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				String text = passwordEditText.getText().toString();
+				if ("forever".equals(text)) {
+					startActivity(new Intent(mActivity, StartActivity.class));
+					LaunchActivity.this.finish();
+				}
+			}
+			
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count,
+					int after) {
+				
+			}
+			
+			@Override
+			public void afterTextChanged(Editable s) {
+				
+			}
+		});
 	}
 
 	private void startNextAnimation() {
@@ -82,8 +110,10 @@ public class LaunchActivity extends BaseActivity implements AnimationListener{
 			textView.setAnimation(textAnimation);
 			textAnimation.start();
 		} else {
-			startActivity(new Intent(mActivity, StartActivity.class));
-			LaunchActivity.this.finish();
+//			startActivity(new Intent(mActivity, StartActivity.class));
+//			LaunchActivity.this.finish();
+			
+			passwordEditText.setVisibility(View.VISIBLE);
 		}
 	}
 	
@@ -103,9 +133,9 @@ public class LaunchActivity extends BaseActivity implements AnimationListener{
 	
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
-		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			return true;
-		}
+//		if (keyCode == KeyEvent.KEYCODE_BACK) {
+//			return true;
+//		}
 		return super.onKeyDown(keyCode, event);
 	}
 }
